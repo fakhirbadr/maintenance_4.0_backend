6bb030846dff04aadb0a08aeace946c367ad068a
fakhirbadr <badrfakhir3@gmail.com>
2024-12-02 12:44:11 +0100

    change cors origin2

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://scxassetmanagement.netlify.app/", // Votre application déployée
+      "https://scxassetmanagement.netlify.app", // Votre application déployée
     ],
   })
 );
